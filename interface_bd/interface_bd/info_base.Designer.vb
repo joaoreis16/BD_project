83efffb6f3f12fd8549fd0c2847deb302a4daebd
VascoRegal <60277746+VascoRegal@users.noreply.github.com>
2021-06-15 01:03:05 +0100

    info_base start

--- interface_bd/interface_bd/info_base.Designer.vb
+++ interface_bd/interface_bd/info_base.Designer.vb
@@ -22,9 +22,93 @@ Partial Class info_base
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(info_base))
+        Me.menuBar = New System.Windows.Forms.GroupBox()
+        Me.homeBttn = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.goBack = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.menuBar.SuspendLayout()
+        CType(Me.homeBttn, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.goBack, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'menuBar
+        '
+        Me.menuBar.BackColor = System.Drawing.SystemColors.Desktop
+        Me.menuBar.Controls.Add(Me.homeBttn)
+        Me.menuBar.Controls.Add(Me.PictureBox1)
+        Me.menuBar.Controls.Add(Me.goBack)
+        Me.menuBar.Location = New System.Drawing.Point(0, 0)
+        Me.menuBar.Name = "menuBar"
+        Me.menuBar.Size = New System.Drawing.Size(1086, 92)
+        Me.menuBar.TabIndex = 32
+        Me.menuBar.TabStop = False
+        '
+        'homeBttn
+        '
+        Me.homeBttn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.homeBttn.Image = CType(resources.GetObject("homeBttn.Image"), System.Drawing.Image)
+        Me.homeBttn.Location = New System.Drawing.Point(546, 34)
+        Me.homeBttn.Name = "homeBttn"
+        Me.homeBttn.Size = New System.Drawing.Size(47, 42)
+        Me.homeBttn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.homeBttn.TabIndex = 19
+        Me.homeBttn.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(958, 26)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(107, 59)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 18
+        Me.PictureBox1.TabStop = False
+        '
+        'goBack
+        '
+        Me.goBack.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.goBack.Image = CType(resources.GetObject("goBack.Image"), System.Drawing.Image)
+        Me.goBack.Location = New System.Drawing.Point(40, 34)
+        Me.goBack.Name = "goBack"
+        Me.goBack.Size = New System.Drawing.Size(47, 42)
+        Me.goBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.goBack.TabIndex = 14
+        Me.goBack.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(244, 220)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 20)
+        Me.Label1.TabIndex = 33
+        Me.Label1.Text = "Label1"
+        '
+        'info_base
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1077, 632)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.menuBar)
+        Me.Name = "info_base"
         Me.Text = "info_base"
+        Me.menuBar.ResumeLayout(False)
+        CType(Me.homeBttn, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.goBack, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents menuBar As GroupBox
+    Friend WithEvents homeBttn As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents goBack As PictureBox
+    Friend WithEvents Label1 As Label
 End Class
